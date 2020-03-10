c03654c9a45402d40747486858feccefa8915b0f
Chris Whiting <chris@bridgeft.com>
2020-03-10 10:48:46 -0600

    Add base models

--- main/inventory/models.py
+++ main/inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+
+
